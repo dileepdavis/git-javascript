27ba2cde7327e69d56561a4ea2353f523c973557
dileepdavis <dileepdavis31@gmail.com>
2020-01-09 18:26:48 -0500

    created a simple file

---
+++ script.js
@@ -0,0 +1,12 @@
+var name='Dileep Davis'
+console.log(`Hello, my name is ${name}.`)
+const number = 4
+console.log(`seclected integer = ${number}`);
+const multi = 4*2
+console.log(`Multiplying ${number} by 2 = ${multi}`);
+const multiply = multi*5
+console.log(`Multiplying ${multi} by 5 = ${multiply}`);
+const divide = multiply/number
+console.log(`Dividing ${multiply} by ${number} = ${divide}`);
+const subtract = divide-7
+console.log(`subtracting ${divide} by 7 = ${subtract}`);
